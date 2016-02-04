ea715768a66ee14b291eaaf7d54c54b523b78853
p00ria <amir1300sobatdar@gmail.com>
2016-02-04 15:50:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {175636120},--Sudo users
+    sudo_users = {175636120,178393183},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
